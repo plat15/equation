7099f705d3c1da3a0d55a6faff06d203b209f65c
plat15 <tripla1505@gmail.com>
2023-03-30 00:42:43 +0700

    Phuong_trinh_bac_1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int a;
+        int b;
+        double x = 0;
+        String kq = null;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Nhập số a: ");
+        a = sc.nextInt();
+
+        System.out.println("Nhập số b: ");
+        b = sc.nextInt();
+
+        if (a == 0) {
+            if (b == 0) {
+                kq = "Phương trình có vô số nghiệm.";
+            } else {
+                kq = "Phương trình vô nghiệm.";
+            }
+        } else {
+            kq = String.valueOf(x = (double) -b / a);
+        }
+
+        System.out.println("=======================================================");
+        System.out.println("Phương trình bạn vừa nhập là: " + a + "x " + b + " = 0");
+        System.out.println("Nghiệm của phương trình là: " + kq);
+        System.out.println("=======================================================");
+
     }
 }
